e00050cb91f11eed6851d888acbfda2a3d3b368b
Pratap chauhan <pratap@starkflow.co>
2020-06-11 16:20:16 +0530

    last commit

--- README.md
+++ README.md
@@ -25,3 +25,16 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Dependencies
+
+For map , AgmCoreModule use 
+In module.ts file , add the google map key
+
+Api used , change the api url as per you own config
+
+## Run Server
+
+npm install 
+ng serve
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB54Lbi1eNUDyYXY1Bvoqt3VIUO1B7uuvs',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
